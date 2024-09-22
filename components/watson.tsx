fe925d187f3766a65eacdc559bd127a532a5da99
adithyakb10 <adithyakb93@gmail.com>
2024-09-22 12:25:34 +0530

    feat: Add chatbot integration

---
+++ components/watson.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+
+"use client";
+
+import {
+  WebChatCustomElement,
+  setEnableDebug,
+} from "@ibm-watson/assistant-web-chat-react";
+// import { useEffect, useState } from "react";
+
+interface WatsonProps {
+  integrationID?: string;
+  serviceInstanceID?: string;
+}
+
+// Include this if you want to get debugging information from this library. Note this is different than
+// the web chat "debug: true" configuration option which enables debugging within web chat.
+setEnableDebug(true);
+
+export default function Watson({
+  integrationID,
+  serviceInstanceID,
+}: WatsonProps) {
+  //   const [integrationID, setIntegrationID] = useState(0);
+  //   const [serviceInstanceID, setServiceInstanceID] = useState(0);
+  //   useEffect(() => {
+  //     setIntegrationID(process.env.integrationID);
+  //     setServiceInstanceID(process.env.serviceInstanceID);
+  //   }, []);
+  const webChatOptions = {
+    integrationID: integrationID,
+    region: "eu-de",
+    serviceInstanceID: serviceInstanceID,
+    openChatByDefault: true,
+    showLauncher: false,
+    themeConfig: {
+      corners: "square",
+    },
+    headerConfig: {
+      hideMinimizeButton: true,
+    },
+    // subscriptionID: 'only on enterprise plans',
+    // Note that there is no onLoad property here. The WebChatContainer component will override it.
+    // Use the onBeforeRender or onAfterRender prop instead.
+  };
+
+  return (
+    <WebChatCustomElement
+      className="MyCustomElement"
+      config={webChatOptions}
+    />
+  );
+}
